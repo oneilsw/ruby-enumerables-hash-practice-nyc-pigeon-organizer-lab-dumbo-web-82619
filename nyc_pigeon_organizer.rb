8fec9487f02e26b07cda70e12c35b8bb23d8404e
Oneil Williams <oneilswilliams@gmail.com>
2019-08-25 21:53:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each do |key,value_hash|
+    if key == :gender
+      data[key].each do |pigeon_key,pigeon_array|
+        pigeon_array.each do |pigeon| 
+             binding.pry 
+          new_hash[pigeon] = {}
+         
+        end  
+      end  
+     
+    end 
+  end  
+  
+  new_hash 
 end
